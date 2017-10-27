7e6026ab528d8943f75c99862e70dc856a405b9d
Soren Goyal <sogoyal@expedia.com>
2017-10-26 19:30:55 -0500

    solved binary search tree

--- leetcode/03-smallest-range/solution.py
+++ leetcode/03-smallest-range/solution.py
@@ -1,13 +1,45 @@
-class Solution:
-    inf = 10 ** 6
-    def smallestRange(self, nums):
-        a = min([s[0] for s in nums])
-        b = max([s[-1] for s in nums])
-        I = [(0,len(s)-1) for s in nums]
-        return self.L(a,b, I, nums)
+class Interval:
+    def __init__(self, a, b):
+        if a > b:
+            raise Exception
+        self.a = a
+        self.b = b
 
-    def isInvalid(self, I):
-        return [i for i in I if i is None]
+    def length(self):
+        return b-a
+
+    def covers(self, L):
+
+class Solution:
+    coverMatrix = []
+    inf = 10 ** 6
+    R1 = {}
+    R2 = {}
+    def smallestRange(self, nums):
+        self.LL = nums
+        a = min([L[0] for L in nums])
+        b = max([L[-1] for L in nums])
+        return self.L(a,b)
+
+    def L(self, a, b):
+        if a > b:
+            return self.inf
+        if a < -10**5 or a > 10**5 or b < 10**5 or b > 10**5:
+            return self.inf
+        r1 = self.getR1(a)
+        r2 = self.getR2(b)
+        if I.covers()
+    def buildR1(self, a):
+        if a in self.R1:
+            return self.R1[a]
+        mins = []
+        for L in self.LL:
+            fl = filter(lambda x: x>=a, L)
+            x = min(fl)
+
+    def buildCoverMatrix(self):
+
+
 
     def updateInterval(self, x1, x2, I, nums):
         I1 = []
@@ -41,6 +73,7 @@ class Solution:
                 if min > val:
                     min = val
         return min
+
 
 if __name__ == '__main__':
     sln = Solution()
