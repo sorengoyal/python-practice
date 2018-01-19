b4b3958b30716c4e6f1dadaf545b5d43099f83a4
Soren Goyal <sorengoyal@gmail.com>
2018-01-18 22:29:54 -0600

    arithmetic slicing in O(N)

--- leetcode/23-arithmetic-slices/solution.py
+++ leetcode/23-arithmetic-slices/solution.py
@@ -7,30 +7,17 @@ class Solution:
         n = len(A)
         if n < 3:
             return 0
-        self.T = [[-1]*n for i in range(n)]
-        self.A = A
-        self.isArithmeticSlice(0,n-1)
         count = 0
-        for i in range(n):
-            for j in range(n):
-                if self.T[i][j] == 1:
-                    count += 1
-        return count
+        inc = 1
+        for i in range(2, n):
+            if 2*A[i-1] - A[i-2] - A[i]:
+                inc = 1
+            else:
+                count += inc
+                inc += 1
 
-    def isArithmeticSlice(self,i,j):
-        T = self.T
-        A = self.A
-        if T[i][j] != -1:
-            return T[i][j]
-        if j - i == 2:
-            T[i][j] = 0 if 2*A[i+1]-A[i]-A[j] else 1
-            return T[i][j]
-        elif j - i > 2:
-            T[i][j] = 0 if self.isArithmeticSlice(i,j-1) + self.isArithmeticSlice(i+1,j) < 2 else 1
-            return T[i][j]
-        else:
-            raise Exception('Invalid case')
+        return count
 
 if __name__ == '__main__':
     sln = Solution()
-    print(sln.numberOfArithmeticSlices([1, 1, 2, 5, 7]))
+    print(sln.numberOfArithmeticSlices([1, 2, 3, 4, 5, 7, 9]))
